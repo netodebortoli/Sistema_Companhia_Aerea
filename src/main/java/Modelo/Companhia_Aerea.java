6229a4897f52d05448277999dfdafe20dc9653bc
Aristides Debortoli Neto <netodebortoli77@gmail.com>
2023-03-10 21:16:10 -0300

    Iniciando o projeto da Companhia Aerea

---
+++ src/main/java/Modelo/Companhia_Aerea.java
@@ -0,0 +1,13 @@
+
+package Modelo;
+
+/**
+ *
+ * @author Admin
+ */
+public class Companhia_Aerea {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
